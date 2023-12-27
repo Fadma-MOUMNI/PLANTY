e59952d1000b5b6910c42cd801f6d073c4c4b554
FADMA MOUMNI <fadmamoumni@MacBook-Pro-de-FADMA.local>
2023-12-27 19:06:23 +0100

    integration des maquetes

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -1,15 +1,29 @@
 <?php
 
-add_action('wp_enqueue_script', 'theme_enqueue_styles');
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
-    wp_enqueue_style('parent-style', get_template_directory().'/style.cs');
+    
+    wp_enqueue_style('parent-style', get_template_directory().'/style.css');
 
-    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css'));
+    wp_enqueue_style('theme-style-2', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+
 }
 
 
+function generateMenu($items, $args) {
+    // Vérifier si l'utilisateur est connecté
+    if (is_user_logged_in()) {
+        // Si l'utilisateur est connecté, ajouter le lien "Admin" 
+        $items =  $items .'<li id="admin"><a href="' . admin_url() . '">Admin</a></li>' ;
+    }
 
+    // Retourner le menu final
+    return $items;
+}
 
+// le filtre pour manipuler le menu
+add_filter('wp_nav_menu_items', 'generateMenu', 10, 2);
 
 ?>
